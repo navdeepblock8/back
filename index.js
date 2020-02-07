bd70abca37d12fe644ca05e3b172492f4007943f
navdeepblock8 <navdeep.duvedi@block8.com>
2020-02-07 17:09:30 +0530

    commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var cors = require('cors')
+var express = require('express');
+var app = express();
+app.use(cors());
+
+
+app.get('/pp',function(req,res){
+    i=Math.round(Math.random() * Math.floor(10),0);
+    if(i>5){
+    res.send(JSON.stringify("You won a Lottery"));
+    }
+    else
+    {
+        res.send(JSON.stringify("Sorry Next Time"));
+    }
+    
+});
+
+
+app.listen(3000);
+exports.app = functions.https.onRequest(app)
